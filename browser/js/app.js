cfe04e44621a288fc9b1a7080e30265e478c7fa9
tackypinkflamingos <ajstocchetti@gmail.com>
2015-11-11 21:36:10 -0500

    boo hashtag urls

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,3 +1,10 @@
 'use strict';
 // window.app = angular.module('FullstackGeneratedApp', ['ui.router', 'ui.bootstrap', 'fsaPreBuilt', 'angularPayments']);
 window.app = angular.module('SubwayFinder', ['ui.router']);
+
+app.config(function ($urlRouterProvider, $locationProvider) {
+    // This turns off hashbang urls (/#about) and changes it to something normal (/about)
+    $locationProvider.html5Mode(true);
+    // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
+    $urlRouterProvider.otherwise('/');
+});
